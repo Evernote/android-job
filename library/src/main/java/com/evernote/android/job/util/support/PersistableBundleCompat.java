b7b17505ee0dbc8cb4b85ef8794406d1d9ae586b
Ralf Wondratschek <r.wondr@gmail.com>
2016-03-14 11:49:13 +0100

    Catch another error on a rooted device

--- library/src/main/java/com/evernote/android/job/util/support/PersistableBundleCompat.java
+++ library/src/main/java/com/evernote/android/job/util/support/PersistableBundleCompat.java
@@ -242,8 +242,8 @@ public final class PersistableBundleCompat {
             // shouldn't happen
             return "";
 
-        } catch (VerifyError e) {
-            // https://gist.github.com/vRallev/9444359f05259e4b6317
+        } catch (Error e) {
+            // https://gist.github.com/vRallev/9444359f05259e4b6317 and other crashes on rooted devices
             CAT.e(e);
             return "";
 
