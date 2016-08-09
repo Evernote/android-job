c99d0265c4393d8568ca938e9d75d63245525f71
Ralf Wondratschek <r.wondr@gmail.com>
2016-08-09 17:00:49 +0200

    Add Stetho

--- demo/src/main/java/com/evernote/android/job/demo/App.java
+++ demo/src/main/java/com/evernote/android/job/demo/App.java
@@ -3,6 +3,7 @@ package com.evernote.android.job.demo;
 import android.app.Application;
 
 import com.evernote.android.job.JobManager;
+import com.facebook.stetho.Stetho;
 
 /**
  * @author rwondratschek
@@ -12,6 +13,8 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Stetho.initializeWithDefaults(this);
+
         JobManager.create(this).addJobCreator(new DemoJobCreator());
     }
 }
