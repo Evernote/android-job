8a3ac7b4177eff15516534066bf141ffb12308bd
Ralf Wondratschek <r.wondr@gmail.com>
2016-10-17 10:30:45 +0200

    Add notification on demo app

--- demo/src/main/java/com/evernote/android/job/demo/DemoSyncJob.java
+++ demo/src/main/java/com/evernote/android/job/demo/DemoSyncJob.java
@@ -1,8 +1,16 @@
 package com.evernote.android.job.demo;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.NotificationCompat;
 
 import com.evernote.android.job.Job;
+
+import java.util.Random;
 
 /**
  * @author rwondratschek
@@ -15,6 +23,24 @@ public class DemoSyncJob extends Job {
     @NonNull
     protected Result onRunJob(final Params params) {
         boolean success = new DemoSyncEngine(getContext()).sync();
+
+        if (params.isPeriodic()) {
+            PendingIntent pendingIntent = PendingIntent.getActivity(getContext(), 0, new Intent(getContext(), MainActivity.class), 0);
+
+            Notification notification = new NotificationCompat.Builder(getContext())
+                    .setContentTitle("Job Demo")
+                    .setContentText("Periodic job ran")
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.ic_notification)
+                    .setShowWhen(true)
+                    .setColor(Color.GREEN)
+                    .setLocalOnly(true)
+                    .build();
+
+            NotificationManagerCompat.from(getContext()).notify(new Random().nextInt(), notification);
+        }
+
         return success ? Result.SUCCESS : Result.FAILURE;
     }
 }
